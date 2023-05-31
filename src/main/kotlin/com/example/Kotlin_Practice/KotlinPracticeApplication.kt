66d1f00ba1e6bf8999be383890367f9fd0cdac94
han <hcsy1219@gmail.com>
2023-05-31 20:54:21 +0900

    Kotlin Variable

--- src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
+++ src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
@@ -1,11 +1,43 @@
 package com.example.Kotlin_Practice
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class KotlinPracticeApplication
+typealias PeopleSet = Set<Exam>
 
 fun main(args: Array<String>) {
-	runApplication<KotlinPracticeApplication>(*args)
+
+	//create valuable
+	val declarationNumber1 = 25
+	val declarationNumber2: Int
+
+	val people: PeopleSet
+
+	val people1 = Exam("Kim",4885)
+	people1.name = "Lee"
+
+	val people2: Exam
+	val number2 = 100
+
+	//basic if
+	//if (declarationNumber1<number2){
+	//	people2 = Exam("Han",4007)
+	//}else{
+	//	people2 = Exam("Joe",1234)
+	//}
+
+	//kotlin's new if
+	people2 = if (declarationNumber1<number2){
+		Exam("Han",4007)
+	}else{
+		Exam("Joe",1234)
+	}
+
+	//auto catch mismatch
+	//number = "hello"
+
+	//kotlin StringBuilder, but actually using java.lang.StringBuilder
+	//StringBuilder
+}
+
+//create class
+class Exam(var name: String, val id: Int){
+
 }
