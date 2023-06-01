658d2d5306e8d11bccccbab802ae4f6aa9e71632
han <hcsy1219@gmail.com>
2023-06-01 21:13:17 +0900

    Kotlin string

--- src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
+++ src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
@@ -35,9 +35,33 @@ fun main(args: Array<String>) {
 
 	//kotlin StringBuilder, but actually using java.lang.StringBuilder
 	//StringBuilder
+
+	println(people1)
+	//before = com.example.Kotlin_Practice.Exam@65b3120a
+	//after = People(name=Lee, id=4885)
+
+	val change = 4.22
+	println("To show the value of change, we use $change") //insert value into string
+
+	val numerator = 10.99
+	val denominator = 20.00
+	println("The value of $numerator divided by $denominator is ${numerator/denominator}")
+
+	println("His id is ${people1.id}")
+
+	val filePath = """c:\somedir\somewhere""" //triple quoted string
+	//don't escape backslash
+
+	val nationalAnthem = """동해 물과 백두산이 마르고 닳도록
+							|하느님이 보우하사 우리나라 만세
+							|무궁화 삼천리 화려 강산
+							|대한 사람 대한으로 길이 보전하세""".trimMargin() //string line alignment
+	println(nationalAnthem)
 }
 
 //create class
 class Exam(var name: String, val id: Int){
-
+	override fun toString(): String {
+		return "People(name=$name, id=$id)" //insert parameter into string
+	}
 }
