8d9d3a79132b07b9819a82dc4a2f8205835beedc
han <hcsy1219@gmail.com>
2023-06-01 21:54:09 +0900

    Kotlin Datatype

---
+++ src/main/kotlin/com/example/Kotlin_Practice/DataTypes.kt
@@ -0,0 +1,32 @@
+package com.example.Kotlin_Practice
+
+class DataTypes {
+    fun exam(){
+        val myInt = 10
+
+        var myLong: Long = 22
+        val myLong2 = 22L
+
+        //myLong = myInt -> not change datatype automatically
+        myLong = myInt.toLong()
+
+        val myByte:Byte = 111
+        var myShort: Short
+        myShort = myByte.toShort()
+
+        var myDouble = 65.987
+        println(myDouble is Double) //print true, or false
+
+        val myFloat = 838.123f
+        println("This is a float: ${myFloat is Float}")
+        myDouble = myFloat.toDouble()
+
+        val char = 'b'
+        val myCharInt = 65
+        println(myCharInt.toChar()) //A
+
+        val myBoolean = true
+        println(JavaBooleanCheck().isWorkWithJava(myBoolean)) //working
+        //when use java class in kotlin, call java class to method style
+    }
+}
---
+++ src/main/kotlin/com/example/Kotlin_Practice/JavaBooleanCheck.java
@@ -0,0 +1,7 @@
+package com.example.Kotlin_Practice;
+
+public class JavaBooleanCheck {
+    public String isWorkWithJava(boolean tf){
+        return tf ? "working":"not working";
+    }
+}
