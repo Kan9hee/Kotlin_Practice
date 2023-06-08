16c906951de7dd93985801807a5cb27634433fa0
han <hcsy1219@gmail.com>
2023-06-08 21:25:39 +0900

    Kotlin companion object

--- src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
+++ src/main/kotlin/com/example/Kotlin_Practice/KotlinPracticeApplication.kt
@@ -1,6 +1,12 @@
 package com.example.Kotlin_Practice
 
 fun main(args: Array<String>) {
-	println(Singleton.getTagLine())
-	println(Singleton.getCopyrightLine())
+	println(Object.getTagLine())
+	println(Object.getCopyrightLine())
+	println(SomeClass.accessPrivateVar())
+
+	val someClass1 = SomeClass.justAssign("this is the string as is")
+	val someClass2 = SomeClass.upperOrLower("this is the string as is",false)
+	println(someClass1.someString)
+	println(someClass2.someString)
 }
---
+++ src/main/kotlin/com/example/Kotlin_Practice/Object.kt
@@ -0,0 +1,38 @@
+package com.example.Kotlin_Practice
+
+import java.time.Year
+
+object Object {
+
+    //singleton
+    //common design pattern in Java, like static
+    //use when you want only one instance of a class during application execution
+    //no constructor
+
+    val currentYear = Year.now().value
+
+    fun getTagLine() = "Our tag text"
+    fun getCopyrightLine() = "Copyright \u00A9 $currentYear. All rights reserved"
+}
+
+class SomeClass private constructor(val someString: String){
+    //companion
+    //Kotlin know that you've got a companion object and there is a function called access private var
+    companion object{
+        private var privateVar=6
+
+        fun accessPrivateVar() = "I'm accessing privateVar: $privateVar"
+
+        //factory pattern - method
+        //prevent anything from outside the class from using a constructor directly,
+        //it has to go through one of the functions in the companion object
+        fun justAssign(str: String) = SomeClass(str)
+        fun upperOrLower(str: String, lowercase: Boolean): SomeClass{
+            if (lowercase){
+                return SomeClass(str.lowercase())
+            }else{
+                return SomeClass(str.uppercase())
+            }
+        }
+    }
+}
