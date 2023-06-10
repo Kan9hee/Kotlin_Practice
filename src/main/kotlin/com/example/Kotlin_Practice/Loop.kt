15ebf9a4ba6c138fd236ad90d70f70274edf3515
han <hcsy1219@gmail.com>
2023-06-10 16:31:35 +0900

    Kotlin loop

--- src/main/kotlin/com/example/Kotlin_Practice/Loop.kt
+++ src/main/kotlin/com/example/Kotlin_Practice/Loop.kt
@@ -24,6 +24,34 @@ class Loop{
         val stepThree = stepRange.step(3) //count by 3
         val reversedRange = stepRange.reversed() //reverse
 
+        //loop - for
+        //kotlin's for loop declares differently than java's for loop
+        for(i in range){ println(i) }
 
+        val str="example"
+        val sRange = 0..str.length
+        for(c in str){ println(c) }
+        for(num in 1..20 step 4){ println(num) }
+        for(num in 20 downTo 1 step 2){ println(num) }
+        for(i in 1 until 10){ println(i) } //1~9
+
+        val seasons = arrayOf("spring","summer","fall","winter")
+        for(index in seasons.indices){ println("${seasons[index]} is season number $index") }
+        println("whatever" !in seasons)
+
+        seasons.forEach{ println(it) } //lambda
+        seasons.forEachIndexed{index,value -> println("$value is season number $index")}
+
+        //loop naming
+        for(i in 1..3){
+            println("loop i")
+            jloop@ for(j in 1..4){
+                println("loop j")
+                for(k in 5..10){
+                    println("loop k")
+                    if(k == 7) { break@jloop }
+                }
+            }
+        }
     }
 }
